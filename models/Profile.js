31e38e828b90ba7f2146737f7499c80a1079155f
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-10 23:28:38 +0100

    Added creating / Updating of profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-    users:{
+    user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     }, 
