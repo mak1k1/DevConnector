6070046625a9cd24d486c3866ae27c3fddd60fab
mak1k1 <martin.cehelsky.ja@gmail.com>
2019-12-14 14:26:23 +0100

    Added login form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -14,13 +15,13 @@ const Register = () => {
         ...formData, [e.target.name]: e.target.value
     })
 
-    const onSubmit = e => {
+    const onSubmit = async(e) => {
         e.preventDefault();
         if(password !==password2){
             console.log('Passwords do not match');
         }
         else {
-            console.log(formData);
+            console.log('Success');
         }
     }
 
@@ -58,7 +59,7 @@ const Register = () => {
           <input type="submit" className="btn btn-primary" value="Register" />
         </form>
         <p className="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
+          Already have an account? <Link to="\login">Sign In</Link>
         </p>
         </Fragment>
     )
